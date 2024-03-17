b96f24fc429976f62174e8cea5908d2aadf78447
Christian Roese <croese@8thlight.com>
2024-03-17 09:40:55 -0400

    initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SEACAT = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SEACAT.abilities = {
+  str: 'SEACAT.Ability.Str.long',
+  dex: 'SEACAT.Ability.Dex.long',
+  con: 'SEACAT.Ability.Con.long',
+  int: 'SEACAT.Ability.Int.long',
+  wis: 'SEACAT.Ability.Wis.long',
+  cha: 'SEACAT.Ability.Cha.long',
+};
+
+SEACAT.abilityAbbreviations = {
+  str: 'SEACAT.Ability.Str.abbr',
+  dex: 'SEACAT.Ability.Dex.abbr',
+  con: 'SEACAT.Ability.Con.abbr',
+  int: 'SEACAT.Ability.Int.abbr',
+  wis: 'SEACAT.Ability.Wis.abbr',
+  cha: 'SEACAT.Ability.Cha.abbr',
+};
